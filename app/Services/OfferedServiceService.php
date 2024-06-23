6640017dd6ca86dbbf369d4f0a784f9e93ecfbbe
Jody H <154429715+Grimjo23@users.noreply.github.com>
2024-06-23 13:25:15 +0200

    feat/add new service

---
+++ app/Services/OfferedServiceService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+
+use App\Interface\OfferedServiceRepositoryInterface;
+use App\Models\OfferedService;
+use Illuminate\Database\Eloquent\Collection;
+
+class OfferedServiceService
+{
+    public function __construct(
+        protected OfferedServiceRepositoryInterface $offeredServiceRepository
+    ) {
+    }
+
+    /**
+     * Get all offered services.
+     */
+    public function getAllOfferedServices(): Collection
+    {
+        return $this->offeredServiceRepository->getAllOfferedServices();
+    }
+
+    /**
+     * Create a new offered service.
+     */
+    public function createOfferedService(array $data): OfferedService
+    {
+        return $this->offeredServiceRepository->createOfferedService($data);
+    }
+
+    /**
+     * Get an offered service by ID.
+     */
+    public function getOfferedService(int|string $id): OfferedService
+    {
+        return $this->offeredServiceRepository->getOfferedServiceById($id);
+    }
+
+    /**
+     * Update an existing offered service.
+     */
+    public function updateOfferedService(array $data, OfferedService $service): OfferedService
+    {
+        return $this->offeredServiceRepository->updateOfferedService($data, $service);
+    }
+
+    /**
+     * Delete an offered service.
+     */
+    public function deleteOfferedService(OfferedService $service): void
+    {
+        $this->offeredServiceRepository->deleteOfferedService($service);
+    }
+}
