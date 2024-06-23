68efa98192fa0d174160ca46cb4df0a8c53f9c43
Jody H <154429715+Grimjo23@users.noreply.github.com>
2024-06-23 02:45:06 +0200

    feat/add new relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -51,13 +51,13 @@ class User extends Authenticatable
         return 'api';
     }
 
-    public function customer(): HasOne
+    public function customerAppointments(): HasMany
     {
-        return $this->hasOne(Customer::class);
+        return $this->hasMany(Appointment::class, 'customer_id');
     }
 
-    public function staff(): HasOne
+    public function staffAppointments(): HasMany
     {
-        return $this->hasOne(Staff::class);
+        return $this->hasMany(Appointment::class, 'staff_id');
     }
 }
